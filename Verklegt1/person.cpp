3dba3c7620b1f91e288790789418619ef92d1af6
svanurtho15 <svanurtho15@ru.is>
2015-11-29 00:57:30 +0000

    Revert "Þurfti að bæta við people skjalinu við til að laga hjá mér"
    This reverts commit c2e2723e47185b037d2385a874b4f777010a297e.

---
+++ Verklegt1/person.cpp
@@ -0,0 +1,63 @@
+#include "person.h"
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+// Constructor - With date of death
+Person::Person(string n, string g, string dob, string dod, string c){
+    Name = n;
+    Gender = g;
+    DateOfBirth = dob;
+    DateOfDeath = dod;
+    Country = c;
+}
+
+// Get set name
+string Person::getName(){
+    return Name;
+}
+void Person::setName(std::string n){
+    Name = n;
+}
+
+// Get set gender
+string Person::getGender(){
+    return Gender;
+}
+void Person::setGender(string g){
+    Gender = g;
+}
+
+// Get set date of birth
+string Person::getDateOfBirth(){
+    return DateOfBirth;
+}
+void Person::setDateOfBirth(string dob){
+    DateOfBirth = dob;
+}
+
+// Get set date of death
+string Person::getDateOfDeath(){
+    return DateOfDeath;
+}
+void Person::setDateOfDeath(string dod){
+    DateOfDeath = dod;
+}
+
+// Get set country
+string Person::getCountry(){
+    return Country;
+}
+
+void Person::setCountry(string c){
+    Country = c;
+}
+
+void Person::display(int nameLength) {
+     cout << "| ";
+     cout << setw(nameLength) << getName() << " | ";
+     cout << setw(6) << getGender() << " | ";
+     cout << setw(10) << getDateOfBirth() << " | ";
+     cout << setw(10) << getDateOfDeath() << " | ";
+     cout << getCountry() << endl;
+}
