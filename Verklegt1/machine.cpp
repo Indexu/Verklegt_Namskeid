774bd9942f778d496beb558da58cb702c478e9b9
Haukur Halldorsson <haukur7@gmail.com>
2015-12-03 15:50:49 +0000

    Added Machine class

---
+++ Verklegt1/machine.cpp
@@ -0,0 +1,56 @@
+#include "machine.h"
+
+
+Machine::Machine(int id, int y, bool b, std::string n, std::string t){
+    Id = id;
+    Year = y;
+    Built = b;
+    Name = n;
+    CType_ID = t;
+}
+
+// GETTERS/SETTERS
+// Get/set machine ID
+int Machine::getId() {
+    return Id;
+}
+
+void Machine::setId(int id) {
+    Id = id;
+}
+
+// Get/set year build year
+int Machine::getYear() {
+    return Year;
+}
+
+void Machine::setYear(int y) {
+    Year = y;
+}
+
+// Get/set if machine is built
+bool Machine::isBuilt() {
+    return Built;
+}
+
+void Machine::isBuilt(bool b) {
+    Built = b;
+}
+
+// Get/set machine name
+std::string Machine::getName() {
+    return Name;
+}
+
+void Machine::setName(std::string n) {
+    Name = n;
+}
+
+// Get/set machine type
+std::string Machine::getType() {
+    return CType_ID;
+}
+
+void Machine::setType(std::string id) {
+    CType_ID = id;
+}
---
+++ Verklegt1/machine.h
@@ -0,0 +1,40 @@
+#ifndef MACHINE_H
+#define MACHINE_H
+#include <string>
+using namespace std;
+
+class Machine
+{
+    int Id;
+    int Year;
+    bool Built;
+    std::string Name;
+    std::string CType_ID;
+public:
+    // Constructor
+    Machine(int id, int y, bool b, std::string n, std::string t);
+
+    // GETTERS/SETTERS
+    // Get/set machine ID
+    int getId();
+    void setId(int id);
+
+    // Get/set year build year
+    int getYear();
+    void setYear(int y);
+
+    // Get/set if machine is built
+    bool isBuilt();
+    void isBuilt(bool b);
+    // Get/set machine name
+    std::string getName();
+    void setName(std::string n);
+
+    // Get/set machine type
+    std::string getType();
+    void setType(std::string t);
+
+};
+
+#endif // MACHINE_H
+
