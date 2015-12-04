17cc6c6a46d7db1a89c3e490fd35bee4b54b0218
Haukur Halldorsson <haukur7@gmail.com>
2015-12-04 16:09:02 +0000

    Fixed edit error

--- Verklegt1/presentation.cpp
+++ Verklegt1/presentation.cpp
@@ -50,6 +50,7 @@ void loop(){
 
                     // List
                     if(getCommand(command) == "ls"){
+                        message = "";
 
                         // If only "ls"
                         if(command == "ls"){
